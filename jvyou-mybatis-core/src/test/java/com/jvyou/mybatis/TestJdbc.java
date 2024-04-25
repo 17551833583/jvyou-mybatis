d2cdb841348d6de987f242df12a5fc9a831c68a0
橘柚 <29892960@qq.com>
2024-04-25 20:05:30 +0800

    测试原生的 JDBC 接口

---
+++ jvyou-mybatis-core/src/test/java/com/jvyou/mybatis/TestJdbc.java
@@ -0,0 +1,40 @@
+package com.jvyou.mybatis;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * @author 橘柚
+ * @version 1.0-SNAPSHOT
+ * @Date 2024/4/25 19:59
+ * @Description 测试原生的 JDBC 接口
+ */
+public class TestJdbc {
+
+    @Test
+    public void testJdbc() throws Exception {
+        // 1.加载数据库驱动
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        // 2.获取数据库链接
+        String url = "jdbc:mysql://127.0.0.1:3306/jvyou-mybatis?useUnicode=true&characterEncoding=UTF8&useSSL=false";
+        Connection connection = DriverManager.getConnection(url, "root", "123456");
+        // 3.执行数据库操作
+        PreparedStatement ps = connection.prepareStatement("select * from t_user where id=?");
+        ps.setInt(1, 2);
+        ps.execute();
+        // 4.获取结果集
+        ResultSet resultSet = ps.getResultSet();
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("name") + "--" + resultSet.getInt("age"));
+        }
+        // 5.关闭数据库链接
+        resultSet.close();
+        ps.close();
+        connection.close();
+    }
+
+}
