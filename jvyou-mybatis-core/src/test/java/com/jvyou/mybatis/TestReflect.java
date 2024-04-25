7273b1c6542f165fca0edac6ab4c2db57b45958a
橘柚 <29892960@qq.com>
2024-04-25 23:13:39 +0800

    测试反射技术

---
+++ jvyou-mybatis-core/src/test/java/com/jvyou/mybatis/TestReflect.java
@@ -0,0 +1,26 @@
+package com.jvyou.mybatis;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author 橘柚
+ * @version 1.0-SNAPSHOT
+ * @Date 2024/4/25 22:22
+ * @Description 测试反射
+ */
+public class TestReflect {
+
+    @Test
+    void testReflect() throws Exception {
+        // 获取类对象
+        Class<?> clazz = Class.forName("com.jvyou.mybatis.TestJdbc");
+        // 获取构造方法
+        // 获取无参构造方法
+        // Constructor<?> constructor = clazz.getConstructor();
+        // 获取有参构造方法
+        // Constructor<?> constructor = clazz.getConstructor(String.class);
+        // 获取所有构造方法
+        // Constructor<?>[] constructors = clazz.getConstructors();
+    }
+
+}
