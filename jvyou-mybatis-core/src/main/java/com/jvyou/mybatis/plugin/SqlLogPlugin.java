5dbc04e853888716799014393c2186f326f7cc1c
橘柚 <29892960@qq.com>
2024-05-14 14:39:23 +0800

    feat:通过结果集插件实现SQL日志输出

--- jvyou-mybatis-core/src/main/java/com/jvyou/mybatis/plugin/SqlLogPlugin.java
+++ jvyou-mybatis-core/src/main/java/com/jvyou/mybatis/plugin/SqlLogPlugin.java
@@ -1,9 +1,10 @@
 package com.jvyou.mybatis.plugin;
 
-import com.jvyou.mybatis.executor.SqlExecutor;
+import com.jvyou.mybatis.executor.resultset.ResultSetHandler;
 import com.jvyou.mybatis.mapping.BoundSql;
 import com.jvyou.mybatis.mapping.MappedStatement;
 
+import java.sql.PreparedStatement;
 import java.util.Properties;
 
 /**
@@ -13,18 +14,17 @@ import java.util.Properties;
  * @Description SQL 日志打印插件
  */
 @Intercepts({
-        @Signature(type = SqlExecutor.class, method = "query", args = {MappedStatement.class, Object.class}),
-        @Signature(type = SqlExecutor.class, method = "update", args = {MappedStatement.class, Object.class})
+        @Signature(type = ResultSetHandler.class, method = "handleResultSets", args = {MappedStatement.class, PreparedStatement.class}),
 })
 public class SqlLogPlugin implements PluginInterceptor {
 
-    
+
     @Override
     public Object intercept(Invocation invocation) {
         System.out.println("日志插件----开始打印日志");
         BoundSql boundSql = ((MappedStatement) invocation.getArgs()[0]).getBoundSql();
-        System.out.println("SQL:" + (boundSql.getParsedSql()));
-        System.out.println("参数:" + (boundSql.getParamNames()));
+        PreparedStatement ps = (PreparedStatement) invocation.getArgs()[1];
+        System.out.println(ps);
         Object result = invocation.proceed();
         System.out.println("日志插件----结束打印日志");
         return result;
